175887443580523536d9664f1ab03b7af63bdc78
Kyler-Bass <kylerbass72@gmail.com>
2023-11-29 14:09:47 -0600

    Initial commit

---
+++ graphics.py
@@ -0,0 +1,139 @@
+import pygame as pg
+import os
+
+class graphics_handler:
+
+  def __init__(self, window, screen_surface):
+
+    # load images here, use convert_alpha for images with transparent pixels
+    self.board_image = pg.image.load('images/board.png').convert()
+    self.red_cir_image = pg.image.load('images/redCircle.png').convert_alpha()
+    self.black_cir_image = pg.image.load('images/blackCircle.png').convert_alpha()
+    self.start_bg = pg.image.load("images/start_bg.png").convert()
+    self.start_button = pg.image.load("images/start_button.png").convert_alpha()
+
+    # start menu objects # 
+    self.start_button_rect = pg.Rect(330, 380, 130, 60) # hitbox for start button
+
+    # window and screen surface objects
+    self.window = window
+    self.main_screen_surface = screen_surface
+
+    # variables
+    self.offset = 70  # how much x and y padding to add to the circle images when placing them onto the screen
+    self.button_size = 140
+
+    # for animation of gameover text 
+    self.gameover_text_vertical = 351
+    self.travelling_down = False
+    self.text = None
+
+    # for upper bounce animation  
+    self.upperbounce = False
+    self.current_bounce = 100
+    self.max_width = 0
+
+    # for lower bounce animation 
+    self.lowerbounce = False
+    self.current_bounce_lower = 100
+    self.max_width_lower = 0
+
+
+  def display(self, board: list[list[str]]):
+    """display the gameboard to the window, input the list representing the board"""
+
+    # add background board to screen
+    self.main_screen_surface.blit(self.board_image, (0, 0))
+
+    # board size = 800x800 px
+    # button size = 140x140 px
+    # padding = 70x70 px
+
+    # add pieces to box
+    for y, row_list in enumerate(board): # y pos in list
+      for x, char in enumerate(row_list): # x pos in list
+        if char == "b":
+          self.main_screen_surface.blit(self.black_cir_image,(x * self.button_size + self.offset, y * self.button_size + self.offset))
+        elif char == "r":
+          self.main_screen_surface.blit(self.red_cir_image,(x * self.button_size + self.offset, y * self.button_size + self.offset))
+        elif char == "":
+          pass
+        else:
+          pass
+
+  
+  def gameover(self, winner:str): 
+    """Screen displayed when game is over, pass in the winner"""
+
+    # fill screen with black
+    self.main_screen_surface.fill((0,0,0))
+
+    # label sizes:
+    # 50 px vertical, 200(red)-250(black) px wide
+
+    # for animation
+    if self.gameover_text_vertical <= 350:
+      self.travelling_down = True
+      self.upperbounce = True
+      self.max_width = 0
+    elif self.gameover_text_vertical >= 400:
+      self.travelling_down = False
+      self.lowerbounce = True
+      self.max_width_lower = 0
+
+    if self.travelling_down:
+      self.gameover_text_vertical += 0.05
+    elif not self.travelling_down:
+      self.gameover_text_vertical -= 0.05
+    
+    # color and borderwidth for bounce animation
+    if winner == "red":
+      color = (255,0,0)
+      border_width = 0
+    else:
+      color = (255,255,255)
+      border_width = 2
+
+    # bounce animation
+    if self.upperbounce:
+      current_width = self.max_width/self.current_bounce
+      self.max_width = 200
+      bounce_variable = self.current_bounce / 400
+      bounce_rect = pg.Rect(400 - current_width/2, 340, current_width, 10)
+      pg.draw.rect(self.main_screen_surface, color, bounce_rect, border_radius=6, width=border_width)
+      self.current_bounce -= bounce_variable
+      if self.current_bounce < 1:
+        self.upperbounce = False
+        self.current_bounce = 100
+    if self.lowerbounce:
+      current_width_lower = self.max_width_lower/self.current_bounce_lower
+      self.max_width_lower = 200
+      bounce_variable_lower = self.current_bounce_lower / 400
+      bounce_rect_lower = pg.Rect(400 - current_width_lower/2, 450, current_width_lower, 10)
+      pg.draw.rect(self.main_screen_surface, color, bounce_rect_lower, border_radius=6, width=border_width)
+      self.current_bounce_lower -= bounce_variable_lower
+      if self.current_bounce_lower < 1:
+        self.lowerbounce = False
+        self.current_bounce_lower = 100
+
+
+    # text
+    if self.text == None:
+      if winner == "red":
+        self.text = pg.image.load(f"images/red_wins.png").convert()
+      elif winner == "black": 
+        self.text = pg.image.load(f"images/black_wins.png").convert() 
+
+    if winner == "red":
+      self.main_screen_surface.blit(self.text, (300,self.gameover_text_vertical))
+    elif winner == "black":
+      self.main_screen_surface.blit(self.text, (275,self.gameover_text_vertical))
+      
+  def start_menu(self, mouse_pos, mouse_pressed):
+    """Displays the start screen, returns True when the start button is pressed"""
+
+    self.main_screen_surface.blit(self.start_bg, (0,0)) # 800x800 px 
+    self.main_screen_surface.blit(self.start_button, (330,380)) # 65x30 px
+
+    if self.start_button_rect.collidepoint(mouse_pos) and mouse_pressed[0]:
+      return True
---
+++ main.py
@@ -0,0 +1,101 @@
+from board import board_class
+from functions import exit_event_check
+import pygame as pg
+import time
+
+
+
+def main():
+
+  # initialize pygame module 
+  pg.init()
+  
+  # create window and screen surface 
+  SCREENSIZE = (800,800)
+  window = pg.display
+  screen_surface = window.set_mode(SCREENSIZE)  
+  window.set_caption("Teeko")
+  icon = pg.image.load("images/icon.png").convert_alpha()
+  window.set_icon(icon)
+  
+  # variables
+  winner = ''
+  gameRunning = True
+  gameover_screen = True
+  start_menu_running = True
+  board_list = [
+            ["","","","",""],
+            ["","","","",""],
+            ["","","","",""],
+            ["","","","",""],
+            ["","","","",""]]
+  
+
+  # initialize board that holds everything
+  board_obj = board_class(board_list, window, screen_surface)
+    
+
+  # start menu
+  while start_menu_running: 
+        if exit_event_check():
+            gameRunning = False
+            gameover_screen = False
+            start_menu_running = False
+        
+        mouse_pos = pg.mouse.get_pos()
+        mouse_pressed = pg.mouse.get_pressed()
+        start_clicked = board_obj.graphics.start_menu(mouse_pos,mouse_pressed)
+
+        if start_clicked:
+           start_menu_running = False
+
+        window.update()
+
+
+  # prevents clicking the middle button in the same instant as the screen switches to the gameboard
+  time.sleep(0.2)
+
+  # gameloop 
+  while gameRunning:
+    # check if user presses exit (x in top right)
+    if exit_event_check():
+        gameRunning = False
+        gameover_screen = False
+
+    # check if someone won here
+    win = board_obj.check_win()
+    if win != 0:
+        if win == 1:
+           gameRunning = False
+           winner = "red"
+        elif win == 2:
+           gameRunning = False
+           winner = "black"
+
+    # display board to user 
+    board_obj.graphics.display(board_obj.board_list)
+
+    # check if the user clicked and grab the mouse position
+    mouse_pressed = pg.mouse.get_pressed()
+    mouse_pos = pg.mouse.get_pos()
+
+    # check if a button was clicked on, and 
+    current_time = time.time()
+    board_obj.check_for_clicks(mouse_pos, mouse_pressed, current_time)
+
+    # update the frame 
+    window.update() 
+
+
+  # gameover screen
+  while gameover_screen:
+    if exit_event_check():
+        gameover_screen = False
+    board_obj.graphics.gameover(winner)
+    window.update()
+    window.flip()
+
+
+
+if __name__ == "__main__":
+    main()
